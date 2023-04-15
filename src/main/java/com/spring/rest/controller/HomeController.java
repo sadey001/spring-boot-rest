a3990ae3f869d93d688c1392a33d25b78845f783
sadey001 <deysanat.ubuntu@gmail.com>
2023-04-15 17:08:25 +0530

    Initial code push

---
+++ src/main/java/com/spring/rest/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.spring.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+	@GetMapping("/test")
+	public String helloAPI() {
+		return "This API is UP and RUNNING";
+	}
+}
